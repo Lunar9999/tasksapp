ded489d776e465ffa247dff3ada1d3d301f86d07
Eva Njoroge <87692414+Lunar9999@users.noreply.github.com>
2025-03-24 01:45:36 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */ #Added some files
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
